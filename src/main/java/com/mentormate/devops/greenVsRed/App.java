5be17c2ace6b30858a2b74ddf88f62fd89936b9d
Preslav Karanedeff <karanedeff@gmail.com>
2020-07-24 20:04:25 +0300

    init

---
+++ src/main/java/com/mentormate/devops/greenVsRed/App.java
@@ -0,0 +1,13 @@
+package com.mentormate.devops.greenVsRed;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/mentormate/devops/greenVsRed/AppTest.java
@@ -0,0 +1,15 @@
+package com.mentormate.devops.greenVsRed;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.*;
+import org.junit.jupiter.api.Assertions;
+/*
+ * Unit test for simple App.
+ */
+public class AppTest {
+	void tests() {
+		
+	}
+   
+}
